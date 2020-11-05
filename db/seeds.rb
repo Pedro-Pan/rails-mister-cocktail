8412224018be41c90206d20f5a6bab808ca9d5ae
Pedro Vilarinho <Pedro.Vilarinho.a@outlook.com>
2020-11-05 23:19:38 +0100

    Added select2 and form onShow page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+
+require 'open-uri'
+ingredients = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+json = JSON.parse(ingredients.read)
+json["drinks"].each { |ingredient| Ingredient.create(name: ingredient["strIngredient1"]) }
